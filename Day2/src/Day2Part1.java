3573819cd4bfc1411b5f18a1ed16489dc61907ae
Calvin Chung <calcchung@gmail.com>
2025-12-02 21:47:23 -0600

    Day2Part2 complete

--- Day2/src/Day2Part1.java
+++ Day2/src/Day2Part1.java
@@ -9,9 +9,8 @@ public class Day2Part1 {
         // "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
         // "824824821-824824827,2121212118-2121212124";
         String stringID = "67562556-67743658,62064792-62301480,4394592-4512674,3308-4582,69552998-69828126,9123-12332,1095-1358,23-48,294-400,3511416-3689352,1007333-1150296,2929221721-2929361280,309711-443410,2131524-2335082,81867-97148,9574291560-9574498524,648635477-648670391,1-18,5735-8423,58-72,538-812,698652479-698760276,727833-843820,15609927-15646018,1491-1766,53435-76187,196475-300384,852101-903928,73-97,1894-2622,58406664-58466933,6767640219-6767697605,523453-569572,7979723815-7979848548,149-216";
-
         String[] stringIDs = stringID.split(",");
-        List<Long> numbersWithMatchingHalves = new ArrayList<>();
+        List<Long> numbersWithMatches = new ArrayList<>();
         long sumInvalidIds = 0;
 
         System.out.println(java.util.Arrays.toString(stringIDs));
@@ -20,41 +19,60 @@ public class Day2Part1 {
             long min = Long.parseLong(parts[0]);
             long max = Long.parseLong(parts[1]);
 
-            // System.out.println("For range " + min + "-" + max);
-            long totalNumbersInRange = getTotalNumbersInRange(min, max);
-            // System.out.println("Total numbers in range: " + totalNumbersInRange);
-
-            // for each number within the range do
             while (min <= max) {
                 long numberToCheck = min;
-                // System.out.println("Checking number: " + numberToCheck);
 
-                long numDigits = numberOfDigits(numberToCheck);
+                int numDigits = numberOfDigits(numberToCheck);
                 String numberString = String.valueOf(numberToCheck);
-                // System.out.println("Number " + numberToCheck + " has " + numDigits + "
-                // digits.");
-                // if numberString has even number of digits, split it in half
-                if (hasEvenNumberOfDigits(numberToCheck)) {
-                    int halfLength = numberString.length() / 2;
-                    String firstHalf = numberString.substring(0, halfLength);
-                    String secondHalf = numberString.substring(halfLength);
-                    // System.out.println("First half: " + firstHalf + ", Second half: " +
-                    // secondHalf);
-                    if (firstHalf.equals(secondHalf)) {
-                        // System.out.println("Found matching halves: " + firstHalf + " and " +
-                        // secondHalf);
-                        System.out.println(firstHalf + secondHalf);
-                        numbersWithMatchingHalves.add(Long.parseLong(firstHalf + secondHalf));
+                List<Integer> divisors = getDivisors(numDigits);
+
+                for (int divisor : divisors) {
+                    if (divisor == numDigits) {
+                        continue;
+                    }
+                    int chunkSize = divisor;
+                    List<String> chunks = splitByChunkSize(numberString, chunkSize);
+                    if (checkChunksForMatch(chunks)) {
+                        System.out.println("Number " + numberToCheck + " has matching chunks of size " + chunkSize);
+                        numbersWithMatches.add(numberToCheck);
+                        break; // no need to check other divisors
                     }
                 }
                 min++;
             }
         }
-        for (long number : numbersWithMatchingHalves) {
+        for (long number : numbersWithMatches) {
             sumInvalidIds += number;
         }
         System.out.println("Sum of numbers with matching halves: " + sumInvalidIds);
 
+    }
+
+    public static boolean checkChunksForMatch(List<String> chunks) {
+        if (chunks.isEmpty()) {
+            return false;
+        }
+        String firstChunk = chunks.get(0);
+        return chunks.stream().allMatch(chunk -> chunk.equals(firstChunk));
+    }
+
+    public static List<String> splitByChunkSize(String str, int chunkSize) {
+        List<String> chunks = new ArrayList<>();
+        for (int i = 0; i < str.length(); i += chunkSize) {
+            chunks.add(str.substring(i, Math.min(i + chunkSize, str.length())));
+        }
+        return chunks;
+    }
+
+    public static List<Integer> getDivisors(int number) {
+        List<Integer> divisors = new ArrayList<>();
+        divisors.add(1);
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                divisors.add(i);
+            }
+        }
+        return divisors;
     }
 
     public static boolean hasEvenNumberOfDigits(long number) {
@@ -71,7 +89,7 @@ public class Day2Part1 {
         return count;
     }
 
-    public static long numberOfDigits(long number) {
+    public static int numberOfDigits(long number) {
         return String.valueOf(Math.abs(number)).length();
     }
 
